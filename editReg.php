02558bcab03c998d3ed877616ac38d24049daad9
ErikVanLeonard <mrerik97@gmail.com>
2021-06-25 16:47:49 -0500

    Modal
    Modal - add

--- editReg.php
+++ editReg.php
@@ -33,7 +33,36 @@ if (isset($_GET['Expediente'])) {
 
 <!-- Page content-->
 <div class="container">
+
+
     <div class="text-center mt-5">
+
+
+        <div class="container-fluid" style="margin-bottom: 30px;">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-4">
+                        </div>
+                        <div class="col-md-4">
+                        </div>
+                        <div class="col-md-4">
+
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Agregar compareciente <i class="fas fa-address-card"></i>
+                            </button>
+                            <button type="button" class="btn btn-info btn-block">
+                                Agregar Inmueble <i class="fas fa-house-user"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
 
         <div class="container-fluid">
             <div class="row">
@@ -82,14 +111,24 @@ if (isset($_GET['Expediente'])) {
                                 <input id="txtFoliosUsados" name="txtFoliosUsados" type="text" value="<?php echo $foliosUsados ?>" class="form-control" readonly>
                             </div>
                         </div>
+
+
+                        
+                                <div class="col-md-12" style="background-color: grey;">
+                                    <h3 class="text-center" style="color: #fff;">
+                                        h3. Lorem ipsum dolor sit amet.
+                                    </h3>
+                                </div>
+                         
+
                         <div class="form-group row" style="margin-bottom: 10px;">
-                            <div>
+                            <div class="d-grid gap-2">
                                 <button name="submit" type="submit" class="btn btn-success btn-block">EnviarXD</button>
                             </div>
                         </div>
 
 
-                        
+
                     </form>
                 </div>
 
@@ -100,7 +139,49 @@ if (isset($_GET['Expediente'])) {
 
 
 
+
+
     </div>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 <?php include "includes/footer.php" ?>
